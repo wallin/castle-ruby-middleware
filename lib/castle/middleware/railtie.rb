698d69a74d1eb7939f540e7b96adc006b03809ae
Sebastian Wallin <sebastian.wallin@gmail.com>
2019-01-05 21:11:49 +0100

    Insert after Rack::Sendfile

--- lib/castle/middleware/railtie.rb
+++ lib/castle/middleware/railtie.rb
@@ -6,9 +6,9 @@ module Castle
   class Middleware
     class Railtie < ::Rails::Railtie
       initializer 'castle.middleware.rails' do |app|
-        app.config.middleware.insert_after ActionDispatch::Static,
+        app.config.middleware.insert_after Rack::Sendfile,
                                            Castle::Middleware::Sensor
-        app.config.middleware.insert_after ActionDispatch::Static,
+        app.config.middleware.insert_after Rack::Sendfile,
                                            Castle::Middleware::Authenticating
         # app.config.middleware.insert_after ActionDispatch::Flash,
         #                                    Castle::Middleware::Tracking
